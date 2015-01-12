740bd85c5d067136076065af459ab5e47855b5b1
Steven Campbell <dukeytoo@gmail.com>
2015-01-11 23:08:30 -0600

    Implemented partial matching of entries

--- popups/masterPasswordController.js
+++ popups/masterPasswordController.js
@@ -18,15 +18,40 @@ function MasterPasswordController($scope, $http, gdocs, keepass) {
 	  $scope.busy = true;
 	  keepass.getPasswords($scope.masterPassword).then(function(entries) {
 	    var url = parseUrl($scope.url);
-	    $scope.entries = entries.filter(function(entry) {
-	      return (entry.URL == url.hostname
-	        || (entry.Title && $scope.title && entry.Title.toLowerCase() == $scope.title.toLowerCase())
-	        || entry.Title == url.hostname
-	        || (entry.URL && url.hostname.indexOf(entry.URL.toLowerCase()) > -1)
-	        || (entry.Title && url.hostname.indexOf(entry.Title.toLowerCase()) > -1)
-	       );
+	    var siteTokens = (url.hostname + "." + $scope.title).toLowerCase().split(/\.|\s|\//);
+	    entries.forEach(function(entry) {
+	      entry.matchRank = 0;
+	      entry.matchRank += (entry.URL == url.hostname) ? 1 : 0;
+	      entry.matchRank += (entry.Title && $scope.title && entry.Title.toLowerCase() == $scope.title.toLowerCase()) ? 1 : 0;
+	      entry.matchRank += (entry.Title == url.hostname) ? 1: 0;
+        entry.matchRank += (entry.URL && url.hostname.indexOf(entry.URL.toLowerCase()) > -1) ? 0.9: 0;
+	      entry.matchRank += (entry.Title && url.hostname.indexOf(entry.Title.toLowerCase()) > -1) ? 0.9 : 0;
+
+	      var entryTokens = (entry.URL + "." + entry.Title).toLowerCase().split(/\.|\s|\//);
+	      for (var i=0; i<entryTokens.length; i++) {
+	        for (var j=0; j<siteTokens.length; j++) {
+	          var token1 = entryTokens[i];
+	          var token2 = siteTokens[j];
+
+	          entry.matchRank += (token1 && token2 && token1 == token2) ? 0.2 : 0;
+	        }
+	      }
 	    });
-	    $scope.successMessage = $scope.entries.length + " matches found";
+
+	    $scope.entries = entries.filter(function(entry) {
+	      return (entry.matchRank > 0.8)
+	    });
+      $scope.successMessage = $scope.entries.length + " matches found";
+	    if ($scope.entries.length == 0) {
+  	    $scope.entries = entries.filter(function(entry) {
+  	      return (entry.matchRank > 0.4);
+  	    });
+	      $scope.successMessage = "No close matches, showing " + $scope.entries.length + " partial matches";
+	    }
+	    if ($scope.entries.length == 0) {
+	      $scope.errorMessage = "No matches found for this site."
+	      $scope.successMessage = "";
+	    }
 	    $scope.busy = false;
 	    $scope.$apply();
 	  }).catch(function(err) {
