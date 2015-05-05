d2c78ad2231d2669bdd16baa34b19a6543a917a5
Steve Campbell <dukeytoo@gmail.com>
2015-05-04 21:21:03 -0500

    Fix ChromeOS 42 not supporting PBKDF2 (fallback to SHA-256)

--- services/secureCacheDisk.js
+++ services/secureCacheDisk.js
@@ -62,32 +62,16 @@ function SecureCacheDisk(protectedMemory, secureCacheMemory, settings) {
           var encoder = new TextEncoder();
           var tokenBytes = encoder.encode(token);
 
-          window.crypto.subtle.importKey(
-						"raw", //only "raw" is allowed
-						tokenBytes, //your password
-						{
-						    name: "PBKDF2",
-						},
-						false, //whether the key is extractable (i.e. can be used in exportKey)
-						["deriveKey"] //can be any combination of "deriveKey" and "deriveBits"
-					).then(function(key){
-						//returns a key object
-						return window.crypto.subtle.deriveKey(
-					    {
-				        "name": "PBKDF2",
-				        salt: salt,
-				        iterations: 100000,
-				        hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-				   		},
-					    key, //your key from generateKey or importKey
-					    { //the key type you want to create based on the derived bits
-				        name: "AES-CBC", //can be any AES algorithm ("AES-CTR", "AES-CBC", "AES-CMAC", "AES-GCM", "AES-CFB", "AES-KW", "ECDH", "DH", or "HMAC")
-				        //the generateKey parameters for that type of algorithm
-				        length: 256, //can be  128, 192, or 256
-					    },
-					    false, //whether the derived key is extractable (i.e. can be used in exportKey)
-					    ["encrypt", "decrypt"] //limited to the options in that algorithm's importKey
-						)
+          //try PBKDF2 if available:
+          window.crypto.subtle.importKey("raw", tokenBytes, {name: "PBKDF2"}, false, ["deriveKey"]).then(function(key){
+						var wantedKeyType = {"name": "PBKDF2",salt: salt,iterations: 100000,hash: {name: "SHA-256"}};
+						var wantedEncryptType = {name: "AES-CBC", length: 256};
+						return window.crypto.subtle.deriveKey(wantedKeyType, key, wantedEncryptType, false, ["encrypt", "decrypt"]);
+					}).catch(function(err) {
+						//fallback to SHA-256 hash if PBKDF2 not supported (ChromeOS, why???)
+						return window.crypto.subtle.digest({name: 'SHA-256'}, tokenBytes).then(function(hash) {		
+	           	return window.crypto.subtle.importKey("raw", hash, AES, false, ['encrypt', 'decrypt']);		
+          	});
 					}).then(function(aesKey) {
 						resolve(aesKey);
 					});
