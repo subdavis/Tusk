1a4e935f767fbbcdee445b83c171762b597052f3
Steve Campbell <dukeytoo@gmail.com>
2016-01-02 12:48:47 -0600

    Improve comments

--- services/streamCipher.js
+++ services/streamCipher.js
@@ -21,9 +21,11 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
  */
 
+/*
+ * Encapsulates decoding and encoding of protected data using the Salsa20 stream cipher.
+ */
 function StreamCipher() {
 	"use strict";
 
