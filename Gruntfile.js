28b1b5708e479b47fbd04d429aa3ce02e4db69b8
Steve Campbell <dukeytoo@gmail.com>
2016-11-26 09:01:08 -0600

    Fix #118 - missing grunt modules in package.json.  Also, update to latest angular 1.4 minor release

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-copy');
