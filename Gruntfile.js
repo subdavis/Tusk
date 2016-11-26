bb2781a6e78eb5dd41bee78d38f746244daabf3b
Steve Campbell <dukeytoo@gmail.com>
2016-11-26 09:14:48 -0600

    Fix lib files created twice in zip

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,7 +85,6 @@ module.exports = function(grunt) {
       target: {
         files: [
           {expand: true, cwd: 'build/', src: '**/*', dest:'/'},
-          {expand: true, src: 'lib/*.js', dest: '/'},
           {expand: true, src: 'manifest.json', dest: '/'},
           {expand: true, src: 'license.txt', dest: '/'},
           {expand: true, src: 'assets/**/*', dest: '/'}
