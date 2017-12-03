7f38a5834c1a99d711bac9a5d24db5a39ec646a4
Brandon Davis <brandon@subdavis.com>
2017-12-03 14:33:15 -0500

    Add icons

--- src/components/Messenger.vue
+++ src/components/Messenger.vue
@@ -1,11 +1,13 @@
 <template>
   <!-- General Messages - Show these all the time -->
-  <div>
+  <div class="messenger">
     <div class="box-bar small warn" v-show="messages.warn">
+      <i class="fa fa-exclamation-triangle fa-fw fa-2x dashicon warn" aria-hidden="true"></i>
       <span>{{ messages.warn }}</span></div>
     <div class="box-bar small error" v-show="messages.error">
       <span>{{ messages.error }}</span></div>
     <div class="box-bar small success" v-show="messages.success">
+      <i class="fa fa-check fa-fw fa-2x" aria-hidden="true"></i>
       <span>{{ messages.success }}</span></div>
   </div>
 </template>
@@ -17,3 +19,13 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss">
+@import "../styles/settings.scss";
+.messenger .fa {
+  float: left;
+  margin: 2px;
+  margin-right: 4px;
+  margin-left: -3px;
+}
+</style>
