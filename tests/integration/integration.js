a5b8823d936a4c125c6aa5ed742bcb379dcaf5e5
zmilonas <25948390+zmilonas@users.noreply.github.com>
2018-09-12 02:12:12 +0200

    Prepare basic view test cases, and first run

--- tests/integration/integration.js
+++ tests/integration/integration.js
@@ -3,8 +3,21 @@ const should = require('should');
 
 const pathToExtension = require('path').join(__dirname, '../..', 'demo-chrome');
 
-describe('chrome browser, user acceptance test', async function() {
-    this.timeout(20000);
+// TODO: Move CSS selectors to a different - mapper - file, to make it more BDD
+
+const extensionUrl = "chrome-extension://fmhmiaejopepamlcjkncpgpdjichnecm/";
+
+const extensionPath = {
+    options: extensionUrl + 'options.html',
+    popup: extensionUrl + 'popup.html',
+};
+
+const delayArg = {
+    delay: 100
+};
+
+describe('user acceptance test', async function() {
+    this.timeout(2 * 60 * 1000);
     let browser;
 
     before(async () => {
@@ -22,12 +35,12 @@ describe('chrome browser, user acceptance test', async function() {
     });
 
     describe('options page', async function() {
-        this.timeout(10000);
+        this.timeout(10 * 1000);
         let page;
 
         before(async () => {
             page = await browser.newPage();
-            await page.goto("chrome-extension://fmhmiaejopepamlcjkncpgpdjichnecm/options.html");
+            await page.goto(extensionPath.options);
         });
 
         after(async () => {
@@ -42,5 +55,54 @@ describe('chrome browser, user acceptance test', async function() {
             (await page.$$("nav")).length.should.be.exactly(1);
         });
 
+        it('should change view after nav click', async () => {
+            const prev = await page.$("#contentbox");
+            const prevBody = await page.evaluate((el) => el.textContent, prev);
+            await page.click(".nav-content > ul > li:not(.active) a", delayArg);
+            await page.waitFor(1000);
+            const next = await page.$("#contentbox");
+            (await page.evaluate((el) => el.textContent, next)).should.not.be.equal(prevBody);
+        });
+
+        it('should have more than one checkbox in manage databases view', async () => {
+            try {
+                await page.click(".nav-content > ul > li:nth-child(2) a", delayArg);
+            } catch (e) {
+                throw new Error("Manage databases nav item not found")
+            }
+            await page.waitFor(500);
+            (await page.$$("input[type='checkbox']")).length.should.be.greaterThan(1);
+        });
     });
+
+    describe('popup', async function() {
+        this.timeout(10 * 1000);
+        let page;
+
+        before(async () => {
+            page = await browser.newPage();
+            await page.goto(extensionPath.popup);
+        });
+
+        after(async () => {
+            await page.close();
+        });
+
+        it('should have only one button', async () => {
+            (await page.$$("button")).length.should.be.exactly(1);
+        });
+
+        it('should have the name of the extension', async () => {
+            const logo = await page.$(".unlockLogo");
+            (await page.evaluate((el) => el.textContent, logo)).includes("KeePass Tusk").should.be.true();
+        });
+
+        it('should lead to options on button click', async () => {
+            await page.click("button", delayArg);
+            await page.waitFor(1000);
+            const openTabs = await browser.pages()
+            openTabs.length.should.be.greaterThan(1);
+            openTabs.filter(a => a.url().indexOf('options.html') > -1).length.should.be.exactly(1);
+        })
+    })
 });
