f3197df0877faa59443481843daff1199c7c5843
subdavis <brandonadavis@outlook.com>
2018-09-29 20:49:17 -0400

    Vuex init

--- src/popup.js
+++ src/popup.js
@@ -6,10 +6,12 @@ require('font-awesome/css/font-awesome.css')
 import Vue from 'vue'
 import Popup from './Popup.vue'
 import VirtualRouter from '$lib/virtual-router.js'
+import store from './store';
 
 // Set up routes
 Vue.prototype.$router = new VirtualRouter()
 
 new Vue({
+	store,
 	render: h => h(Popup)
 }).$mount('#app')
